d3ee2e6a1878996b6c96b9d0cc9e2532cd51924b
Mikołaj Smoleński <smolenski.mikolaj@gmail.com>
2017-08-21 18:54:22 +0200

    browserSync added

--- gruntfile.js
+++ gruntfile.js
@@ -34,13 +34,25 @@ module.exports = function(grunt) {
 		},
 	},
 	
+	browserSync: {
+		bsFiles: {
+			src : 'css/*.css'
+		},
+		options: {
+			server: {
+				baseDir: "./"
+			}
+		}
+	}
+	
   });
   // Load the plugins tasks
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browser-sync');
 
   // Default task(s).
 
-  grunt.registerTask('default', ['sass', 'imagemin', 'watch']);
+  grunt.registerTask('default', ['sass', 'imagemin', 'watch', 'browserSync']);
 };
