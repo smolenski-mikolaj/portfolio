55f611ad33135e488995b1f1efe538d5651d064e
Mikołaj Smoleński <smolenski.mikolaj@gmail.com>
2017-08-25 23:53:00 +0200

    feature

--- gruntfile.js
+++ gruntfile.js
@@ -13,17 +13,6 @@ module.exports = function(grunt) {
   		}
   	},
 
-  	imagemin: {
-  		dynamic: {
-  			files: [{
-  				expand: true,
-  				cwd: 'images/',
-  				src: ['**/*.{png,jpg,gif}'],
-  				dest: 'images/build/'
-  			}]
-  		},
-  	},
-
 	watch: {
 		scripts: {
 			files: ['sass/*.sass'],
@@ -48,11 +37,10 @@ module.exports = function(grunt) {
   });
   // Load the plugins tasks
   grunt.loadNpmTasks('grunt-sass');
-  grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-browser-sync');
 
   // Default task(s).
 
-  grunt.registerTask('default', ['sass', 'imagemin', 'watch', 'browserSync']);
+  grunt.registerTask('default', ['sass', 'watch', 'browserSync']);
 };
